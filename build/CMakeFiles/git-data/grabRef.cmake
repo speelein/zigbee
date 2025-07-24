4bc1fe931a0afa0a530acefae72958fb298723d2
speelein <prufer.g@gmx.net>
2025-07-24 08:58:14 +0200

    neu compeliert

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/mnt/nvme/SSD/Daten/Visual-Studio/C-6/zigbee/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/gunter/esp/v5.5-rc1/esp-idf/.git")
+set(GIT_DIR "/mnt/nvme/SSD/Daten/Visual-Studio/C-6/zigbee/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
